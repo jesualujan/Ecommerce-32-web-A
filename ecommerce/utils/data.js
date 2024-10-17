43be8efbc6b4a8f87c7ffc8fe1d6dbbd69769804
jesualujan <lujan.oficial@gmail.com>
2024-10-16 21:13:02 -0600

    ✨creamos los datos para los productos

---
+++ ecommerce/utils/data.js
@@ -0,0 +1,60 @@
+export const data = {
+    products: [
+        {
+            id: 1,
+            title: 'Product 1',
+            description: 'Product 1 description',
+            price: 100,
+            image
+        },
+        {
+            id: 2,
+            title: 'Product 2',
+            description: 'Product 2 description',
+            price: 200,
+            image
+        },
+        {
+            id: 3,
+            title: 'Product 3',
+            description: 'Product 3 description',
+            price: 300,
+            image
+        },
+        {
+            id: 4,
+            title: 'Product 4',
+            description: 'Product 4 description',
+            price: 400,
+            image
+        },
+        {
+            id: 5,
+            title: 'Product 5',
+            description: 'Product 5 description',
+            price: 500,
+            image
+        },
+        {
+            id: 6,
+            title: 'Product 6',
+            description: 'Product 6 description',
+            price: 600,
+            image
+        },
+        {
+            id: 7,
+            title: 'Product 7',
+            description: 'Product 7 description',
+            price: 700,
+            image
+        },
+        {
+            id: 8,
+            title: 'Product 8',
+            description: 'Product 8 description',
+            price: 800,
+            image
+        },
+    ]
+}

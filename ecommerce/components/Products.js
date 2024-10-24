6c48962979327bdf54199c09b722096c7fe3f600
jesualujan <lujan.oficial@gmail.com>
2024-10-23 19:18:47 -0600

    ✨ creamos la vista del componente products

--- ecommerce/components/Products.js
+++ ecommerce/components/Products.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {data} from '../utils/data'
+import { Image } from '@chakra-ui/react'
+// import Image from 'next/image'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+      {data.products.map((products) => (
+        <Image 
+        key={products._id}
+        src={`/images${products.image}`}
+        alt={products.title}
+        width={230}
+        height={230}
+        style={{objectFit: "cover"}}
+        />
+      ))}
+    </div>
   )
 }
 

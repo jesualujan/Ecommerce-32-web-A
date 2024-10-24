91beeffd3eac1b14081da9ba7f8a73506be8ac02
jesualujan <lujan.oficial@gmail.com>
2024-10-23 19:58:05 -0600

    ✨ quitamos al componente Layout

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,12 +1,12 @@
-import Layout from "../components/Layout"
+
 import Products from "../components/Products"
 
 export default function Home() {
   return (
     <>
-      <Layout>
+   
         <Products/>
-      </Layout>
+   
     </>
   )
 }

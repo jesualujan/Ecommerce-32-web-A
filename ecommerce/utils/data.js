5070572a5f5ee3d7d7eda80405feb92237b5a6d5
jesualujan <lujan.oficial@gmail.com>
2024-10-23 19:19:01 -0600

    ✨ agregue las imagenes a data

--- ecommerce/utils/data.js
+++ ecommerce/utils/data.js
@@ -5,56 +5,56 @@ export const data = {
             title: 'Product 1',
             description: 'Product 1 description',
             price: 100,
-            image
+            image: '/23.jpg'
         },
         {
             id: 2,
             title: 'Product 2',
             description: 'Product 2 description',
             price: 200,
-            image
+            image: '/46.jpg'
         },
         {
             id: 3,
             title: 'Product 3',
             description: 'Product 3 description',
             price: 300,
-            image
+            image: '/72.jpg'
         },
         {
             id: 4,
             title: 'Product 4',
             description: 'Product 4 description',
             price: 400,
-            image
+            image: '/73.jpg'
         },
         {
             id: 5,
             title: 'Product 5',
             description: 'Product 5 description',
             price: 500,
-            image
+            image: '/74.jpg'
         },
         {
             id: 6,
             title: 'Product 6',
             description: 'Product 6 description',
             price: 600,
-            image
+            image: '/75.jpg'
         },
         {
             id: 7,
             title: 'Product 7',
             description: 'Product 7 description',
             price: 700,
-            image
+            image: '/76.jpg'
         },
         {
             id: 8,
             title: 'Product 8',
             description: 'Product 8 description',
             price: 800,
-            image
+            image: '/23.jpg'
         },
     ]
 }

fdfcfef431fa243d5239f900aa11bb6f809a2acb
jesualujan <lujan.oficial@gmail.com>
2024-10-23 19:34:16 -0600

    ✨ creamos la vista de nuestros productos

--- ecommerce/components/Products.js
+++ ecommerce/components/Products.js
@@ -1,23 +1,43 @@
 import React from 'react'
 import {data} from '../utils/data'
 import { Image } from '@chakra-ui/react'
+import {
+  Grid,
+  GridItem,
+  Card,
+  Box
+} from '@chakra-ui/react'
+import  Link  from 'next/link'
 // import Image from 'next/image'
 
 const Products = () => {
   return (
     <div>
-      {data.products.map((products) => (
-        <Image 
-        key={products._id}
-        src={`/images${products.image}`}
-        alt={products.title}
-        width={230}
-        height={230}
-        style={{objectFit: "cover"}}
-        />
-      ))}
+      <Grid templateColumns={{base: "1fr", lg:"repeat(4, 1fr)"}} gap={6}>
+        {data.products.map((products) => (
+          <GridItem key={products.id} colSpan={1}>
+            <Card>
+              <Link href={`/product/${products.id}`}>
+                <Image
+                  key={products._id}
+                  src={`/images${products.image}`}
+                  alt={products.title}
+                  width={230}
+                  height={230}
+                  style={{ objectFit: "cover" }}
+                />
+                <Box py={1} px={1}>
+                  <h3>{products.title}</h3>
+                  <div>{products.description}</div>
+                  <div>${products.price}.00</div>
+                </Box>
+              </Link>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
     </div>
-  )
+  );
 }
 
 export default Products

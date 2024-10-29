d63f30f1f656a6e2fd241203d90b0536f37a8858
jesualujan <lujan.oficial@gmail.com>
2024-10-28 19:37:18 -0600

    ✨ creamos nuestro modelo de productos

---
+++ ecommerce/models/Products.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    id: {type: String, require: true, unique: true},
+    title: {type: String, require: true},
+    image: {type: String, require: true},
+    price: {type: Number, require: true},
+    description: {type: String, require: true}
+},
+ {timestamps: true} // sirve para saber cuando un producto (dato) fue creado
+)
+
+const Product = mongoose.model.Product || mongoose.model('Product', productSchema)
+export default Product

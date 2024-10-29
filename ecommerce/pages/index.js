8fa9555ef217eff91bddfa8d971f64d0049d236a
jesualujan <lujan.oficial@gmail.com>
2024-10-28 19:53:12 -0600

    ✨ consumimos la api de products

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,12 +1,40 @@
-
 import Products from "../components/Products"
+import {useState, useEffect} from 'react'
+
 
 export default function Home() {
+  const[products, setProducts] = useState([])
+  const[loading, setLoading] = useState(false)
+  const[error, setError] = useState(false)
+
+  useEffect(() => {
+    fetchProducts()
+  },[])
+
+ async function fetchProducts(){
+  try{
+      setLoading(true)
+      setError(false)
+      const response = await fetch('/api/products')
+      const newProducts = await response.json()
+      setProducts(newProducts)
+  }catch (err) {
+    setError(true)
+  }
+  setLoading(false)
+ }
+
+  if(error){
+    return <div> Error al cargar</div>
+  }
+
+  if(loading){
+    return <div> Cargando  . .</div>
+  }
+
   return (
-    <>
-   
-        <Products/>
-   
+    <>   
+        <Products data={products}/>
     </>
   )
 }

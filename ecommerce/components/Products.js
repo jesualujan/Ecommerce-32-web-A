9b80e732011069a7aa923f73b92023242a529e50
jesualujan <lujan.oficial@gmail.com>
2024-10-30 19:46:05 -0600

    ✨ consumimos los datos desde la API

--- ecommerce/components/Products.js
+++ ecommerce/components/Products.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {data} from '../utils/data'
+// import {data} from  '../utils/data'
 import { Image } from '@chakra-ui/react'
 import {
   Grid,
@@ -10,21 +10,20 @@ import {
 import  Link  from 'next/link'
 // import Image from 'next/image'
 
-const Products = () => {
+const Products = ({data}) => {
   return (
     <div>
       <Grid templateColumns={{base: "1fr", lg:"repeat(4, 1fr)"}} gap={6}>
-        {data.products.map((products) => (
+        {data.map((products)  => (
           <GridItem key={products.id} colSpan={1}>
             <Card>
               <Link href={`/product/${products.id}`}>
                 <Image
-                  key={products._id}
-                  src={`/images${products.image}`}
-                  alt={products.title}
-                  width={230}
-                  height={230}
-                  style={{ objectFit: "cover" }}
+                 src={`/images${products.image}`}
+                 height={230}
+                 width={230}
+                 style={{objectFit: "cover" , height:"230px"}}
+                 alt={products.title}
                 />
                 <Box py={1} px={1}>
                   <h3>{products.title}</h3>

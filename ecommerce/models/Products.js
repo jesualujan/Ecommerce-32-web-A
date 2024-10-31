08846c7b736ed5855ce5471fc772f0a158397847
jesualujan <lujan.oficial@gmail.com>
2024-10-30 19:45:24 -0600

    🐛 fixed bug

--- ecommerce/models/Products.js
+++ ecommerce/models/Products.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+    {
     id: {type: String, require: true, unique: true},
     title: {type: String, require: true},
     image: {type: String, require: true},
     price: {type: Number, require: true},
     description: {type: String, require: true}
-},
- {timestamps: true} // sirve para saber cuando un producto (dato) fue creado
+    },
+    {
+        timestamps: true
+    } // sirve para saber cuando un producto (dato) fue creado
 )
 
-const Product = mongoose.model.Product || mongoose.model('Product', productSchema)
+const Product = mongoose.models.Product || mongoose.models('Product', productSchema)
 export default Product
+
